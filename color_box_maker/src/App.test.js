2ad01a55b26e5294b0d3382be879b904b601f1c3
sam <wax654@gmail.com>
2021-06-23 12:31:24 -0700

    Finished tests for colored boxes

--- color_box_maker/src/App.test.js
+++ color_box_maker/src/App.test.js
@@ -30,4 +30,28 @@ it("should add a new box to the DOM", () => {
     fireEvent.click(btn);
 
     expect(getByTestId("BoxElement")).toBeInTheDocument();
+});
+
+it("should remove a box from the DOM when it's X is clicked", () => {
+    let boxStats = {};
+
+    const { getByLabelText, getByText, getByTestId, queryByTestId } = render(<App />);
+    //setup START
+    const heightInput = getByLabelText("Height", { exact: false });
+    const widthInput = getByLabelText("Width", { exact: false });
+    const colorInput = getByLabelText("Color", { exact: false });
+
+    const btn = getByText("Make me a Box", { exact: false });
+
+    fireEvent.change(heightInput, { target: { value: "100" } });
+    fireEvent.change(widthInput, { target: { value: "200" } });
+    fireEvent.change(colorInput, { target: { value: "blue" } });
+
+    fireEvent.click(btn);
+    //Setup END
+
+    const deleteBoxBtn = getByText("X");
+    fireEvent.click(deleteBoxBtn);
+
+    expect(queryByTestId("BoxElement")).not.toBeInTheDocument();
 })
--- color_box_maker/src/NewBoxForm.test.js
+++ color_box_maker/src/NewBoxForm.test.js
@@ -11,7 +11,35 @@ it('should match previous snapshot', function () {
     expect(asFragment()).toMatchSnapshot();
 });
 
-it("should allow users to type into the inputs", () =>{
+
+it("should allow users to type into the inputs", () => {
+    let boxStats = {};
+
+    const mockedOnSubmit = boxInfo => boxStats = boxInfo;
+
+    const { getByLabelText, getByText, debug } = render(<NewBoxForm onSubmit={mockedOnSubmit} />);
+
+    //Height input
+    const heightInput = getByLabelText("Height", { exact: false });
+    expect(heightInput.value).toEqual("");
+    fireEvent.change(heightInput, { target: { value: "100" } });
+    expect(heightInput.value).toEqual("100");
+
+    //Width input
+    const widthInput = getByLabelText("Width", { exact: false });
+    expect(widthInput.value).toEqual("");
+    fireEvent.change(widthInput, { target: { value: "200" } });
+    expect(widthInput.value).toEqual("200");
+
+    //Color input
+    const colorInput = getByLabelText("Color", { exact: false });
+    expect(colorInput.value).toEqual("");
+    fireEvent.change(colorInput, { target: { value: "blue" } });
+    expect(colorInput.value).toEqual("blue");
+
+});
+
+it("should pass input data to the passed function when submited", () =>{
     let boxStats = {};
 
     const mockedOnSubmit = boxInfo => boxStats = boxInfo;
@@ -32,6 +60,6 @@ it("should allow users to type into the inputs", () =>{
     fireEvent.click(btn);
 
     expect(boxStats).toEqual({height: "100", width: "200", color: "blue"});
-})
+});
 
 
