ac49c82344e459040bbf58c0d753833f696a6526
sam <wax654@gmail.com>
2021-06-23 07:31:06 -0700

    started Project

---
+++ color_box_maker/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BoxList from './BoxList';
+import "./App.css";
+
+/** Simple app that just shows the LightsOut game. */
+
+function App() {
+  return (
+    <div className="App">
+      <BoxList />
+    </div>
+  );
+}
+
+export default App;
